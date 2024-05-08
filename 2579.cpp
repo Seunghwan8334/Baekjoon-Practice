d8d5285f6167932eff7ef3976173e8d722ebaaf0
Seunghwan Choi <114864057+Seunghwan8334@users.noreply.github.com>
2024-05-08 19:35:22 +0900

    Add files via upload

---
+++ 2579.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int a[303];
+int dp[3][303];
+
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    for (int i=1; i<=n; i++) scanf("%d",&a[i]);
+
+
+    for (int i=1; i<=n; i++) {
+        dp[0][i] = max(dp[1][i-1],dp[2][i-1]);
+        dp[1][i] = dp[0][i-1]+a[i];
+        if (i>=2) dp[2][i] = dp[1][i-1]+a[i];
+    }
+    printf("%d",max(dp[1][n],dp[2][n]));
+}
