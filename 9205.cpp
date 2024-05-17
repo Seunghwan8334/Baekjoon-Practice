cfeb21ea9761c2a6c438438d26a2cd172416eb08
Seunghwan Choi <114864057+Seunghwan8334@users.noreply.github.com>
2024-05-17 20:35:47 +0900

    Add files via upload

---
+++ 9205.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+struct s {
+    int y;
+    int x;
+};
+
+vector <int> v[104];
+queue <int> q;
+s p[104];
+int chk[104];
+
+int main() 
+{
+    int t;
+    scanf("%d",&t);
+    while(t--) {
+        int n;
+        scanf("%d",&n);
+        for (int i=1; i<=n+2; i++) scanf("%d %d",&p[i].y,&p[i].x);
+        for (int i=1; i<=n+2; i++) {
+            for (int j=i+1; j<=n+2; j++) {
+                if (abs(p[i].y-p[j].y)+abs(p[i].x-p[j].x) <= 1000) {
+                    v[i].push_back(j);
+                    v[j].push_back(i);
+                }
+            }
+        }
+        int ans = 0;
+        q.push(1);
+        chk[1] = 1;
+        while(!q.empty()) {
+            int num = q.front();
+            q.pop();
+
+            if (num == n+2) ans = 1;
+            for (int i=0; i<v[num].size(); i++) {
+                if (chk[v[num][i]]) continue;
+                chk[v[num][i]] = 1;
+                q.push(v[num][i]);
+            }
+        }
+
+        if (ans) printf("happy\n");
+        else printf("sad\n");
+
+        while(!q.empty()) q.pop();
+        for (int i=1; i<=102; i++) chk[i] = 0;
+        for (int i=1; i<=102; i++) v[i].clear();
+    }
+}
