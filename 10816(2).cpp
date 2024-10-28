bc4b6513badb20d22665537180c252c4cd381bb1
Seunghwan <114864057+Seunghwan8334@users.noreply.github.com>
2024-10-28 19:23:53 +0900

    Add files via upload

---
+++ 10816(2).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+std::map <int,int> M;
+
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    while(n--) {
+        int tmp;
+        scanf("%d",&tmp);
+        if (M.find(tmp) == M.end()) M.insert({tmp,1});
+        else M[tmp]++;
+    }
+
+    int m;
+    scanf("%d",&m);
+    while(m--) {
+        int tmp;
+        scanf("%d",&tmp);
+        if (M.find(tmp) == M.end()) printf("0 ");
+        else printf("%d ",M[tmp]);
+    }
+}
