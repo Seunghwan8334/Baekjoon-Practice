4188e4fd7b96ffc783ae9477d710918cdf3eff52
Seunghwan <114864057+Seunghwan8334@users.noreply.github.com>
2024-10-30 13:48:19 +0900

    Update 10816(2).cpp

--- 10816(2).cpp
+++ 10816(2).cpp
@@ -5,18 +5,15 @@ std::map <int,int> M;
 
 int main() 
 {
-    int n;
+    int n,m,tmp;
     scanf("%d",&n);
     while(n--) {
-        int tmp;
         scanf("%d",&tmp);
         if (M.find(tmp) == M.end()) M.insert({tmp,1});
         else M[tmp]++;
     }
-    int m;
     scanf("%d",&m);
     while(m--) {
-        int tmp;
         scanf("%d",&tmp);
         if (M.find(tmp) == M.end()) printf("0 ");
         else printf("%d ",M[tmp]);
